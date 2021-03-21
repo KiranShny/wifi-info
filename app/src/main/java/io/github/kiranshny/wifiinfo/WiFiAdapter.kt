76e77dbdc24fb9069eac52ecdf3ff111a41fc997
KiranShny <ikiran1997>
2021-03-21 18:39:42 +0530

    Adds BSSID Copier

--- app/src/main/java/io/github/kiranshny/wifiinfo/WiFiAdapter.kt
+++ app/src/main/java/io/github/kiranshny/wifiinfo/WiFiAdapter.kt
@@ -1,15 +1,20 @@
 package io.github.kiranshny.wifiinfo
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context.CLIPBOARD_SERVICE
 import android.net.wifi.ScanResult
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+
 
 class WiFiAdapter : RecyclerView.Adapter<WiFiAdapter.WiFiViewHolder>() {
 
-    private val data: MutableList<ScanResult>  = mutableListOf()
+    private val data: MutableList<ScanResult> = mutableListOf()
 
     fun setData(results: List<ScanResult>) {
         data.clear()
@@ -32,14 +37,21 @@ class WiFiAdapter : RecyclerView.Adapter<WiFiAdapter.WiFiViewHolder>() {
     }
 
     class WiFiViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val cardTitle: TextView= itemView.findViewById(R.id.ssidTv)
-        val cardSubTitle: TextView= itemView.findViewById(R.id.bssidTv)
-        val cardCounter: TextView= itemView.findViewById(R.id.rssiTv)
+        private val cardTitle: TextView = itemView.findViewById(R.id.ssidTv)
+        private val cardSubTitle: TextView = itemView.findViewById(R.id.bssidTv)
+        private val cardCounter: TextView = itemView.findViewById(R.id.rssiTv)
 
         fun bind(scanResult: ScanResult) {
             cardTitle.text = scanResult.SSID
             cardSubTitle.text = scanResult.BSSID
             cardCounter.text = scanResult.level.toString()
+            cardSubTitle.setOnClickListener {
+                val clipboard: ClipboardManager? =
+                    itemView.context.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager?
+                val clip = ClipData.newPlainText("BSSID", scanResult.BSSID)
+                clipboard?.setPrimaryClip(clip)
+                Toast.makeText(itemView.context, "${scanResult.SSID} BSSID Copied", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
